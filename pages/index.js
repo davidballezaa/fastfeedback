188b74931d5a95bc7acac55a74b90f14da7dfbc8
davidballezaa <davidballezaa@gmail.com>
2022-02-28 22:42:02 -0600

    add styles to index

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,34 @@
 import Head from 'next/head'
-import { Button, Code, Heading, Text } from '@chakra-ui/react'
+import { Flex, Button } from '@chakra-ui/react'
 
 import { useAuth } from '../lib/auth'
+import { LogoIcon } from '../styles/theme'
 
 export default function Home() {
-  const auth = useAuth()
+  const authObj = useAuth()
+
   return (
-    <>
+    <Flex
+      as="main"
+      direction="column"
+      align="center"
+      justify="center"
+      h="100vh"
+    >
       <Head>
         <title>Fast Feedback</title>
       </Head>
 
-      <main>
-        <Heading as="h1" size="4xl">
-          Fast Feedback
-        </Heading>
-
-        <Text>
-          Current user: <Code>{auth.user ? auth.user.email : 'None'}</Code>
-        </Text>
-
-        <div>{auth?.user?.email}</div>
-        {auth.user ? (
-          <Button onClick={e => auth.signout()}>Sign Out</Button>
-        ) : (
-          <Button onClick={e => auth.signinWithGithub()}>Sign In</Button>
-        )}
-      </main>
-    </>
+      <LogoIcon color="black" w={16} h={16} />
+      {authObj.user ? (
+        <Button as="a" href="/dashboard">
+          View Dashboard
+        </Button>
+      ) : (
+        <Button mt={4} size="sm" onClick={e => authObj.signinWithGitHub()}>
+          Sign In
+        </Button>
+      )}
+    </Flex>
   )
 }
