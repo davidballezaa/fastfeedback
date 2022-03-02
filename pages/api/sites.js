0204f3db599cbdd5497f7351d3f60762ea30c622
davidballezaa <davidballezaa@gmail.com>
2022-03-01 18:12:29 -0600

    add api route to fetch all the sites

---
+++ pages/api/sites.js
@@ -0,0 +1,14 @@
+import { database } from '../../lib/db'
+import { collection, getDocs } from 'firebase/firestore'
+
+export default async function handler(req, res) {
+  let sites = []
+  const querySnapshot = await getDocs(collection(database, 'sites'))
+  querySnapshot.forEach(doc => {
+    sites.push({
+      siteId: doc.id,
+      ...doc.data()
+    })
+  })
+  res.status(200).json(sites)
+}
