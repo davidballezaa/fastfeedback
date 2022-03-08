e42021e1663fbffc7d75d3225c3016b49a3e5e64
davidballezaa <davidballezaa@gmail.com>
2022-03-08 13:28:28 -0600

    get rid of dashboard shell and add flex

--- components/FreePlanEmptyState.js
+++ components/FreePlanEmptyState.js
@@ -1,15 +1,20 @@
-import { Box, Button, Heading, Text } from '@chakra-ui/react'
-import DashboardShell from './DashboardShell'
+import { Flex, Button, Heading, Text } from '@chakra-ui/react'
 
 const FreePlanEmptyState = () => {
   return (
-    <DashboardShell>
-      <Box width="100%" backgroundColor="white" borderRadius="8px" p={8}>
-        <Heading size="md">Get feedback on your site instantly.</Heading>
-        <Text>Start today, then grow with us 🌱</Text>
-        <Button>Upgrade to starter</Button>
-      </Box>
-    </DashboardShell>
+    <Flex
+      width="100%"
+      backgroundColor="white"
+      borderRadius="8px"
+      p={8}
+      justify="center"
+      align="center"
+      direction="column"
+    >
+      <Heading size="md">Get feedback on your site instantly.</Heading>
+      <Text>Start today, then grow with us 🌱</Text>
+      <Button>Upgrade to starter</Button>
+    </Flex>
   )
 }
 
