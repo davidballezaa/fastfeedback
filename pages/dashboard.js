ed432c02f8c9d21fd30bcacdc5e50de063a23961
davidballezaa <davidballezaa@gmail.com>
2022-03-09 11:15:35 -0600

    use swr to fetch data  client-side

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,13 +1,26 @@
 import EmptyState from '../components/EmptyState'
-import { useAuth } from '../lib/auth'
+import DashboardShell from '../components/DashboardShell'
+import SiteTableSkeleton from '../components/SiteTableSkeleton'
+import useSWR from 'swr'
+import fetcher from '../lib/fetcher'
+import SiteTable from '../components/SiteTable'
 
 function Dashboard() {
-  const authObj = useAuth()
-  if (!authObj.user) {
-    return 'Loading...'
+  const { data } = useSWR('/api/sites', fetcher)
+
+  if (!data) {
+    return (
+      <DashboardShell>
+        <SiteTableSkeleton />
+      </DashboardShell>
+    )
   }
 
-  return <EmptyState />
+  return (
+    <DashboardShell>
+      {data.sites ? <SiteTable sites={data.sites} /> : <EmptyState />}
+    </DashboardShell>
+  )
 }
 
 export default Dashboard
