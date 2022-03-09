74d3bc28d15678fda265ea58c88ef122572382f2
davidballezaa <davidballezaa@gmail.com>
2022-03-09 15:14:08 -0600

    create dynamic route to show feedback

---
+++ pages/p/[siteId].js
@@ -0,0 +1,67 @@
+import { Box, Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+
+import Feedback from '../../components/Feedback'
+import { useAuth } from '../../lib/auth'
+import { createFeedback, getAllFeedback, getAllSites } from '../../lib/db'
+
+export async function getStaticProps(context) {
+  const { siteId } = context.params
+  const { feedback } = await getAllFeedback(siteId)
+  return {
+    props: {
+      initialFeedback: feedback
+    },
+    revalidate: 5
+  }
+}
+
+export async function getStaticPaths() {
+  const { sites } = await getAllSites()
+  const paths = sites.map(site => ({
+    params: {
+      siteId: site.siteId
+    }
+  }))
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export default function SiteFeedback({ initialFeedback }) {
+  const auth = useAuth()
+  const router = useRouter()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const newFeedback = {
+      author: auth.user.name,
+      authorId: auth.user.uid,
+      createdAt: new Date().toISOString(),
+      provider: auth.user.provider,
+      siteId: router.query.siteId,
+      status: 'pending',
+      text: event.target.feedback.value
+    }
+
+    createFeedback(newFeedback)
+  }
+
+  return (
+    <Box display="flex" flexDirection="column" maxWidth="700px" margin="0 auto">
+      <Box as="form" onSubmit={handleSubmit}>
+        <FormControl>
+          <FormLabel htmlFor="feedback">Feedback</FormLabel>
+          <Input id="feedback" name="feedback" type="text" />
+        </FormControl>
+        <Button type="submit" mt={2}>
+          Add Comment
+        </Button>
+      </Box>
+      {initialFeedback.map(feedback => (
+        <Feedback key={feedback.feedbackId} {...feedback} />
+      ))}
+    </Box>
+  )
+}
