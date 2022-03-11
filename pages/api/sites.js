cd16021643d8b61cf1122b912d9467573f334126
davidballezaa <davidballezaa@gmail.com>
2022-03-10 19:26:04 -0600

    verify id token and display only user's sites

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,9 +1,13 @@
-import { getAllSites } from '../../lib/db'
+import { auth } from '../../lib/firebase-admin'
+
+import { getUserSites } from '../../lib/db'
 
 export default async function handler(req, res) {
-  const { sites, error } = await getAllSites()
-  if (error) {
+  try {
+    const { uid } = await auth.verifyIdToken(req.headers.token)
+    const sites = await getUserSites(uid)
+    res.status(200).json({ sites })
+  } catch (error) {
     res.status(500).json({ error })
   }
-  res.status(200).json({ sites })
 }
