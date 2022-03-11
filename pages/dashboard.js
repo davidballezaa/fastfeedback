b6b05cee7e0643cb16c0cfa5d65bf406f7dcd858
davidballezaa <davidballezaa@gmail.com>
2022-03-10 19:25:38 -0600

    call api route with token if user

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,15 @@
+import useSWR from 'swr'
+
+import { useAuth } from '../lib/auth'
 import EmptyState from '../components/EmptyState'
 import DashboardShell from '../components/DashboardShell'
 import SiteTableSkeleton from '../components/SiteTableSkeleton'
-import useSWR from 'swr'
 import fetcher from '../lib/fetcher'
 import SiteTable from '../components/SiteTable'
 
 function Dashboard() {
-  const { data } = useSWR('/api/sites', fetcher)
+  const { user } = useAuth()
+  const { data } = useSWR(user ? ['/api/sites', user.token] : null, fetcher)
 
   if (!data) {
     return (
